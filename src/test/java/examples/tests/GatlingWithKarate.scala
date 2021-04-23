3e0f356dce90de936843c21b4926382e3391e04a
staffier <70542944+staffier@users.noreply.github.com>
2021-04-23 14:53:05 -0400

    Add files via upload

---
+++ src/test/java/examples/tests/GatlingWithKarate.scala
@@ -0,0 +1,51 @@
+package examples.tests
+
+import com.intuit.karate.gatling.PreDef._
+import io.gatling.core.Predef._
+
+import scala.concurrent.duration._
+import scala.language.postfixOps
+
+class GatlingWithKarate extends Simulation {
+
+  val protocol = karateProtocol()
+
+  val happyPath = scenario("Happy Path Test")
+    .exec(karateFeature("classpath:examples/tests/performance-test.feature@happy-path"))
+
+  val sadPath = scenario("Sad Path Test")
+    .exec(karateFeature("classpath:examples/tests/performance-test.feature@not-so-happy-path"))
+
+  val optionalFields = scenario("Optional Fields")
+    .exec(karateFeature("classpath:examples/tests/performance-test.feature@optional-fields"))
+
+  val allScenarios = scenario("All Scenarios")
+    .exec(karateFeature("classpath:examples/tests/performance-test.feature"))
+
+//  setUp(
+//    happyPath.inject(
+//      rampUsersPerSec(0) to (50) during (10 seconds),
+//      constantUsersPerSec(50) during (30 seconds)
+//    ).protocols(protocol),
+//    sadPath.inject(
+//      rampUsersPerSec(0) to (25) during (10 seconds),
+//      constantUsersPerSec(25) during (30 seconds)
+//    ).protocols(protocol),
+//    optionalFields.inject(
+//      rampUsersPerSec(0) to (25) during (10 seconds),
+//      constantUsersPerSec(25) during (30 seconds)
+//    ).protocols(protocol)
+//  )
+
+  setUp(
+    allScenarios.inject(
+      rampUsersPerSec(0) to (33) during (10 seconds),
+      incrementUsersPerSec(11)
+        .times(5)
+        .eachLevelLasting(10 seconds)
+        .separatedByRampsLasting(5 seconds)
+        .startingFrom(33)
+    )
+  )
+
+}
---
+++ src/test/java/examples/tests/server-starter.sh
@@ -0,0 +1,2 @@
+# This will only work if you have jbang installed
+karate -m ~/KarateDemo/src/test/java/examples/tests/server.feature -p 8080

61b998198e10ebadc7d007cfcb86a660d82e88d7
Anthony Staffier <70542944+staffier@users.noreply.github.com>
2022-03-28 10:29:31 -0400

    Update startup.sh

--- src/test/java/kafka/startup.sh
+++ src/test/java/kafka/startup.sh
@@ -5,6 +5,3 @@ docker-compose -f kafka-single-broker.yml up -d
 docker ps
 echo "*** sleeping for 15 seconds (give time for containers to spin up)"
 sleep 15
-echo "*** the following topic were created ...."
-kcat -L -b localhost:9092
-#kafka-topics -zookeeper 127.0.0.1:2181 --list
